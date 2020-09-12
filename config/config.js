46d50135ede96ff7da677da3dd963fe1be3b2e2d
Daniel Hartnell <danielhartnell@gmail.com>
2020-09-11 22:13:45 -0700

    Using Heroku provided DATABASE_URL

--- config/config.js
+++ config/config.js
@@ -2,10 +2,7 @@ require('dotenv').config();
 
 module.exports = {
 	local: {
-		username: process.env.DATABASE_USER,
-		password: process.env.DATABASE_PASSWORD,
-		database: process.env.DATABASE_NAME,
-		host: process.env.DATABASE_HOST,
+		use_env_variable: "DATABASE_URL",
 		dialect: 'postgres',
 		protocol: 'postgres',
 		dialectOptions: {
@@ -16,10 +13,7 @@ module.exports = {
 		},
 	},
 	production: {
-		username: process.env.DATABASE_USER,
-		password: process.env.DATABASE_PASSWORD,
-		database: process.env.DATABASE_NAME,
-		host: process.env.DATABASE_HOST,
+		use_env_variable: "DATABASE_URL",
 		dialect: 'postgres',
 		protocol: 'postgres',
 		dialectOptions: {
